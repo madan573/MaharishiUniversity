62f3d0acd42f955238e4580fae896e759d073e78
Madan <33192789+madan573@users.noreply.github.com>
2022-11-05 00:23:39 +0545

    prime update

--- prime.java
+++ prime.java
@@ -1,11 +1,11 @@
 public class prime {
     public static void main(String[] args){
-        int start=10, end=30, total=0;
+        int start=20, end=22, total=0;
         total = primeCount(start,end);
         System.out.println(total);
     }
     public static int primeCount(int start,int end){
-        count = 0 ;
+       int count = 0 ;
         for(int i=start;i<=end;i++)
         {
             if(isPrime(i)==1)
@@ -17,12 +17,12 @@ public class prime {
         
         return count;
     }
-    static int isPrime(int i){
+   public static int isPrime(int i){
         if(i<=1)
         {
             return 0;
         }
-        for(int j=2; j=i/2;j++)
+        for(int j=2; j<=i/2;j++)
         {
             if(i%j==0){
                 return 0;
