26ddb6ec3da51f8fc645cc8a9a2b1eab4c6843c6
Madan <33192789+madan573@users.noreply.github.com>
2022-11-05 08:25:27 +0545

    sq pair

---
+++ squarepair.java
@@ -0,0 +1,14 @@
+/*
+ * For example, if the array is {11, 5, 4, 20} the function would return 3 because 
+ * the only square pairs that can be constructed from those numbers are <5, 11>,
+ * <5, 20> and <4, 5>.
+ */
+public class squarepair {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+		
+	}
+
+}
