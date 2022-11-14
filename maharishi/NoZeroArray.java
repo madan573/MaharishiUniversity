9daef2258d5e9bc02801b24ff3b73aef72f10c41
Madan <33192789+madan573@users.noreply.github.com>
2022-11-14 19:11:25 +0545

    program

--- maharishi/NoZeroArray.java
+++ maharishi/NoZeroArray.java
@@ -4,7 +4,19 @@ public class NoZeroArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] a= new int[]{2,4,5,0};
+		int [] a= new int[]{2,4,5,1};
+		int res = arrayHasNoZeros(a);
+		System.out.println(res);
+	}
+	static int arrayHasNoZeros(int[] a) {
+		int r=1;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==0) {
+				r=0;
+				break;
+			}
+		}
+		return r;
 	}
 
 }
