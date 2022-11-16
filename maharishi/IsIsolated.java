54c1c6eb3fe5ad9fa92717bde26e0ad098083733
Madan <33192789+madan573@users.noreply.github.com>
2022-11-16 23:21:35 +0545

    Update IsIsolated.java

--- maharishi/IsIsolated.java
+++ maharishi/IsIsolated.java
@@ -1,10 +1,42 @@
 package maharishi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IsIsolated {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		long n = 67893;
+		int result = isIsolated(n);
+		System.out.println(result);
+	}
+	static int isIsolated(long n) {
+		int ret = 1;
+		long sq=n*n,cu=n*n*n;
+		List<Integer> list = new ArrayList<>();
+		if(n>2097151) {
+			ret=-1;
+			return ret ;
+		}
 		
+		while(sq!=0) {
+			list.add((int) (sq%10));
+			sq = sq/10;
+		}
+		int rem=0;
+		while(cu!=0) {
+			 rem = (int)cu%10;
+			for(int i=0;i<list.size();i++) {
+				if(rem==list.get(i)) {
+					ret = 0;
+					break;
+				}
+			}
+			cu = cu/10;
+		}
+		
+		return ret;
 	}
 
 }
