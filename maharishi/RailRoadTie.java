c794648531f5c46c560c4274562683e698bcf047
Madan <33192789+madan573@users.noreply.github.com>
2022-11-18 00:45:32 +0545

    program

---
+++ maharishi/RailRoadTie.java
@@ -0,0 +1,44 @@
+package maharishi;
+
+public class RailRoadTie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,0,2};
+		int res = isRailRoadTie(a);
+		System.out.println(res);
+	}
+	static int isRailRoadTie(int[] a) {
+		int  result=0;
+		if(a.length==0 || a[0]==0 || a[a.length-1]==0) {
+			result=0;
+		}
+		if(a.length==2 && a[0]!=0 && a[1]!=0) {
+			result =1;
+			return result;
+		}
+		for(int i=1;i<a.length-1;i++) {
+			
+			if(a[i]!=0 && a[i-1]!=0 ) {
+				if(a[i+1]==0) {
+					result=1;
+				}else {
+					result=0;
+				}
+			}
+			if(a[i]!=0 && a[i+1]!=0) {
+				if(a[i-1]==0) {
+					result=1;
+				}else {
+					result=0;
+				}
+			}
+			if(a[i]==0 && a[i-1]!=0 && a[i+1]!=0) {
+				result =1;
+			}
+		}
+		
+		return result;
+	}
+
+}
