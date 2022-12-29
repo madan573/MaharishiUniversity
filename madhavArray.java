2c20619ae66d0eec66e00bd7fe7a088a11e89f4c
Madan <33192789+madan573@users.noreply.github.com>
2022-12-29 20:52:52 +0545

    update

--- madhavArray.java
+++ madhavArray.java
@@ -1,59 +1,38 @@
-import java.util.*;
-
 public class madhavArray{
 
     public static void main(String [] args){
-        final int N =10 ;
-        int[] a =new int[N];
-        int ch = arraySize(N);
-        if(ch==1){
-            System.out.println("Please check the size of array");
-           // System.exit(1);
-        }else{
-        for(int i=0;i<N;i++){
-            Scanner sc = new Scanner(System.in);
-            a[i] = sc.nextInt();
-        }
+    	int[] a= {2,1,1,3,-1,0,7,3,-8,0};
         int r = isMadhavArray(a);
-        if(r==1){
-            System.out.println("This is Madhav array");
-        }else{
-            System.out.println("This is not Madhav array");
-        }
-    }
-    }
-   
-    public static int arraySize(int n){
+        System.out.println(r);
+       }
+      public static int arraySize(int n){
         int r = 0, M=0;
         for (int i=1;i<=n;i++){
             M = i* (i+1)/2;
-            if(M!=n){
+            if(M==n){
                 r=1;
-            }else{
-                r=0;
                 break;
             }
+            if(M>n) break;
         }
         return r;
     }
    public static int isMadhavArray(int[] a)
     {
-        int ret = 1;
         int sum, c=0;
+        if(arraySize(a.length)==1) {
         for(int i=0;i<a.length;i=i+c){
             sum=0;
-            
             for(int j=i;j<=i+c;j++){
                 sum =sum+a[j];
-                //c++;
             }
             if(sum!=a[0]){
-                ret = 0;
-                break;
+                return 0;
             }
             c++;
         }
-    
-        return ret;
+        return 1;
+        }
+        return 0;
     }
 }
--- upcount.java
+++ upcount.java
@@ -2,15 +2,14 @@ public class upcount{
 
     public static void main(String[] args){
         int[] a ={2,3,1,-6,8,-3,-1,2};   
-        int n = 5;
-        int up = nUpCount(a,n);  
-        System.out.println("The upcount of an array is: "+up);
+        int up = nUpCount(a,5);  
+        System.out.println(up);
     }
     public static int nUpCount(int a[], int n){
         int count=0,partialsum=0;
         for(int i=0;i<a.length;i++)
         {
-              if(partialsum<=n && partialsum+a[i]>n){
+              if(partialsum<=n && (partialsum+a[i])>n){
                 count++;
             }
             partialsum += a[i];
