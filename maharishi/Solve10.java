8baec4d2868f236ff14341d20f76c2bc6c8f69a9
Madan <33192789+madan573@users.noreply.github.com>
2022-12-31 14:03:35 +0545

    update

--- maharishi/Solve10.java
+++ maharishi/Solve10.java
@@ -38,4 +38,4 @@ public class Solve10 {
 		}
 		}
 	
-}
+} 
--- maharishi/centerFifteen.java
+++ maharishi/centerFifteen.java
@@ -2,29 +2,21 @@ package maharishi;
 
 public class centerFifteen {
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,10,5,15,1};
+		int[] arr = {1,1,14,2};
 		System.out.println(isCentered15(arr));
 	}
 	public static int isCentered15(int[] a) {
-		int r=0,sum=0,clast=0,cstart=a.length/2-1;
-		if((a.length==3 && a[a.length/2]==15) || (a.length==2 && a[0]+a[1]==15)|| a.length==1 && a[0]==15) {
-			r=1;
-			return r;
-		}
-		if(a.length==0) {
-			return r;
-		}
-		for(int i=a.length/2-1;i<a.length;i++) {
-			if(sum!=15) {
-				sum=sum+a[i];	
-			}else {
-				clast++;
-				if(cstart==clast) {
-					r=1;
-				}
+		int left=0, right=a.length-1,sum=0;
+		while(left<=right) {
+			for(int i=left;i<=right;i++) {
+				sum+=a[i];
 			}
+			if(sum==15) return 1;
+			sum=0;
+			left++;
+			right--;
 		}
-		return r;
+		return 0;
 	}
 
 }
