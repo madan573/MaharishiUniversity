ff149893e144b626223774c9f211f54950874fb3
Madan <33192789+madan573@users.noreply.github.com>
2023-01-01 11:37:33 +0545

    update

--- maharishi/IsAllPossibilities.java
+++ maharishi/IsAllPossibilities.java
@@ -7,13 +7,15 @@ public class IsAllPossibilities {
 		int[] a = {1,0,3,2};
 		System.out.print(isAllPossibilities(a));
 	}
+	
 	static int isAllPossibilities(int [] a) {
 		if(a.length<=0) {
 			return 0;
 		}
 		for(int i=0;i<a.length-1;i++) {
+			if(a[i]<0 || a[i]>=a.length) return 0;
 			for(int j=i+1;j<a.length;j++) {
-				if(a[i]<0 || a[i]>=a.length || a[i]==a[j]) {
+				if(a[i]==a[j]) {
 					return 0;
 				}
 			}
