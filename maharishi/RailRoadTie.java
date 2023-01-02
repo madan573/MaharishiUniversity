2782eb567b9e03c8e64e446c496457f0cae3659c
Madan <33192789+madan573@users.noreply.github.com>
2023-01-02 20:08:05 +0545

    update

--- maharishi/RailRoadTie.java
+++ maharishi/RailRoadTie.java
@@ -4,10 +4,27 @@ public class RailRoadTie {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {1,2};
+		int[] a = {1,0,1,2};
 		int res = isRailRoadTie(a);
 		System.out.println(res);
 	}
+	static int isRailRoadTie(int[] a) {
+		if(a.length<2) return 0;
+		if(a[0]==0 ||a[a.length-1]==0 ) return 0;
+		int count=1;
+		for(int i=0;i<a.length;i++) {
+			if(count==3) {
+				if(a[i]!=0) return 0;
+				count=1;
+				continue;
+			}
+			if(a[i]==0) return 0;
+			count++;
+		}
+		if(count!=3) return 0;
+		return 1;
+	}
+	/*
 	static int isRailRoadTie(int[] a) {
 		int  result=1;
 		if(a.length==0 || a[0]==0 || a[a.length-1]==0) {
@@ -37,5 +54,5 @@ public class RailRoadTie {
 		
 		return result;
 	}
-
+*/
 }
